78cbe77d305c69506654f40e1b7eec805666c9b0
jeete <53453647+jeet@users.noreply.github.com>
2024-12-07 03:18:47 +0530

    WIP https://learn.microsoft.com/en-us/training/modules/interact-with-data-blazor-web-apps/7-exercise-share-data-in-blazor-applications

--- 4th_tut/BlazingPizza/Program.cs
+++ 4th_tut/BlazingPizza/Program.cs
@@ -1,9 +1,12 @@
 using BlazingPizza.Data;
+using BlazingPizza.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+
+builder.Services.AddScoped<OrderState>();
 
 builder.Services.AddHttpClient();
 builder.Services.AddSqlite<PizzaStoreContext>("Data Source=pizza.db");
