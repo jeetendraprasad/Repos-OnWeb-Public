c1f6b636096b00e03e4e9a1d106a1c57f8e191df
jeete <53453647+jeet@users.noreply.github.com>
2024-12-08 16:13:20 +0530

    tik tak toe wip

---
+++ TicTacToeGame/TicTacToeGame/Pages/Board.razor.cs
@@ -0,0 +1,50 @@
+﻿namespace TicTacToeGame.Pages
+{
+    public partial class Board
+    {
+        #region [Game tools]
+        char[,] board =
+        {
+            {' ', ' ', ' ' },
+            {' ', ' ', ' ' },
+            {' ', ' ', ' ' }
+        };
+
+        char player = 'x';
+
+        List<List<int[]>> winningCombinations = new()
+        {
+            // 00 01 02
+            // 10 11 12
+            // 20 21 22
+            new List<int[]>() { new int[] {0, 0}, new int[] {0, 1}, new int[] {0, 2} },
+            new List<int[]>() { new int[] {1, 0}, new int[] {1, 1}, new int[] {1, 2} },
+            new List<int[]>() { new int[] {2, 0}, new int[] {2, 1}, new int[] {2, 2} },
+
+            new List<int[]>() { new int[] {0, 0}, new int[] {1, 0}, new int[] {2, 0} },
+            new List<int[]>() { new int[] {0, 1}, new int[] {1, 1}, new int[] {2, 1} },
+            new List<int[]>() { new int[] {0, 2}, new int[] {1, 2}, new int[] {2, 2} },
+
+            new List<int[]>() { new int[] {0, 0}, new int[] {0, 1}, new int[] {0, 2} },
+            new List<int[]>() { new int[] {1, 0}, new int[] {1, 1}, new int[] {1, 2} },
+            new List<int[]>() { new int[] {2, 0}, new int[] {2, 1}, new int[] {2, 2} },
+
+            new List<int[]>() { new int[] {0, 2}, new int[] {1, 1}, new int[] {2, 2} },
+            new List<int[]>() { new int[] {0, 2}, new int[] {1, 1}, new int[] {2, 0} },
+        };
+        #endregion
+
+        public Board()
+        {
+            
+        }
+
+        private async Task MakeMove(int row, int col)
+        {
+            board[row, col] = player;
+            player = player == 'x' ? 'o' : 'x';
+
+            foreach(var item in winningCombinations) { }
+        }
+    }
+}
