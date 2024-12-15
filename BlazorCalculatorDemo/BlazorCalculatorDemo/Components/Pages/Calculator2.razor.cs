fe1c11c09ccaa2f7ca260aea3b924e37acadb433
jeete <53453647+jeet@users.noreply.github.com>
2024-12-16 01:54:37 +0530

    wup calculator2

--- BlazorCalculatorDemo/BlazorCalculatorDemo/Components/Pages/Calculator2.razor.cs
+++ BlazorCalculatorDemo/BlazorCalculatorDemo/Components/Pages/Calculator2.razor.cs
@@ -2,8 +2,9 @@ namespace BlazorCalculatorDemo.Components.Pages
 {
     public partial class Calculator2
     {
-        public int ElemWidthInPx { get; set; } = 800;
+        public int ElemWidthInPx { get; set; } = 1024;
 
-        public int NoOfCols { get; set; } = 12;
+        public int NoOfRows { get; set; } = 3;
+        public int NoOfCols { get; set; } = 4;
     }
 }
