d6a3c1303806f566799c0515b880ccad9b5353a8
jeete <53453647+jeet@users.noreply.github.com>
2024-12-21 03:11:17 +0530

    calculator state management WIP

--- BlazorCalculatorDemo/BlazorCalculatorDemo/Components/Pages/Calculator2.razor.cs
+++ BlazorCalculatorDemo/BlazorCalculatorDemo/Components/Pages/Calculator2.razor.cs
@@ -4,5 +4,14 @@ namespace BlazorCalculatorDemo.Components.Pages
 {
     public partial class Calculator2
     {
+        //protected override void OnInitialized()
+        //{
+        //    _calculator2State.Volatile.OnChange += StateHasChanged;
+        //}
+
+        //public void Dispose()
+        //{
+        //    _calculator2State.Volatile.OnChange -= StateHasChanged;
+        //}
     }
 }
