8c63ead8cd18c30f66c3a2bc259498a0285cf6b7
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2025-01-08 03:27:15 +0530

    wip

--- BlazorWasmGames2/BlazorWasmGames2/BlazorWasmGames2/Code/SudokuGame.cs
+++ BlazorWasmGames2/BlazorWasmGames2/BlazorWasmGames2/Code/SudokuGame.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorWasmGames2.Pages;
+using System.Diagnostics;
 using System.Globalization;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -6,15 +7,31 @@ namespace BlazorWasmGames2.Code
 {
     internal class SudokuGame
     {
-        Integer1 _rowsBlock = new(2, 2, 3), _colsBlock = new(2, 2, 3);
+        int _rowsBlockMinVal = 2;
+        int _rowsBlockMaxVal = 3;
+        int _rowsBlockStartVal = 2;
+        int _colsBlockMinVal = 2;
+        int _colsBlockMaxVal = 3;
+        int _colsBlockStartVal = 2;
+
+
+        Integer1 _rowsBlock, _colsBlock;
 
         const string _blockIdPrefix = "B[{0},{1}]";                // block names are B[{0-based rows},{0-based cols}]
         const string _cellIdPrefix = "{0}:C[{1},{2}]";             // cell names are BLOCK_ID:[{0-based rows},{0-based cols}]
 
         public SudokuGame()
         {
+            _rowsBlock = new(_rowsBlockStartVal, _rowsBlockMinVal, _rowsBlockMaxVal);
+            _colsBlock = new(_colsBlockStartVal, _colsBlockMinVal, _colsBlockMaxVal);
+
             //Init();
         }
+
+        public int RowsBlockMinVal { get => _rowsBlockMinVal; }
+        public int RowsBlockMaxVal { get => _rowsBlockMaxVal; }
+        public int ColsBlockMinVal { get => _colsBlockMinVal; }
+        public int ColsBlockMaxVal { get => _colsBlockMaxVal; }
 
         public void Resize(int rowsBlock, int colsBlock)
         {
@@ -22,15 +39,25 @@ namespace BlazorWasmGames2.Code
             _colsBlock.ValAsInt = colsBlock;
         }
 
-        public SudokuUi GetSudokuUi()
+        public int GetRowsBlock()
         {
-            SudokuUi sudokuUi = new SudokuUi();
-
-            sudokuUi.RowsBlock = _rowsBlock.ValAsInt;
-            sudokuUi.ColsBlock = _colsBlock.ValAsInt;
-
-            return sudokuUi;
+            return _rowsBlock.ValAsInt;
         }
+
+        public int GetColsBlock()
+        {
+            return _colsBlock.ValAsInt;
+        }
+
+        //public SudokuUi GetSudokuUi()
+        //{
+        //    SudokuUi sudokuUi = new SudokuUi();
+
+        //    sudokuUi.RowsBlock = _rowsBlock.ValAsInt;
+        //    sudokuUi.ColsBlock = _colsBlock.ValAsInt;
+
+        //    return sudokuUi;
+        //}
     }
 
     
@@ -54,9 +81,11 @@ namespace BlazorWasmGames2.Code
         private int GetValue() => _value;
         private int SetValue(int value)
         {
+            Debug.Assert(_minValue <= _maxValue);
+
             if (value < _minValue)
                 _value = _minValue;
-            if (value > _maxValue)
+            else if (value > _maxValue)
                 _value = _maxValue;
             else
                 _value = value;
--- BlazorWasmGames2/BlazorWasmGames2/BlazorWasmGames2/Pages/Sudoku.razor.cs
+++ BlazorWasmGames2/BlazorWasmGames2/BlazorWasmGames2/Pages/Sudoku.razor.cs
@@ -1,5 +1,6 @@
 ﻿using BlazorWasmGames2.Code;
 using Microsoft.AspNetCore.Components;
+using System.Text.Json;
 
 namespace BlazorWasmGames2.Pages
 {
@@ -29,7 +30,20 @@ namespace BlazorWasmGames2.Pages
             else
                 ;
 
-            //_sudokuGame.Resize();
+
+
+            //UpdateUISizeBindings();
+            //_sudokuUi.GridSize = _sudokuUi.RowsBlock * _sudokuUi.ColsBlock;
+            Integer1 rowsBlock1 = new(0, _sudokuGame.RowsBlockMinVal, _sudokuGame.RowsBlockMaxVal);
+            Integer1 colsBlock1 = new(0, _sudokuGame.ColsBlockMinVal, _sudokuGame.ColsBlockMaxVal);
+
+            rowsBlock1.ValAsInt = _sudokuUi.RowsBlock;
+            colsBlock1.ValAsInt = _sudokuUi.ColsBlock;
+
+            _sudokuUi.RowsBlock = rowsBlock1.ValAsInt;
+            _sudokuUi.ColsBlock = colsBlock1.ValAsInt;
+
+            Console.WriteLine(JsonSerializer.Serialize(_sudokuUi));
 
             _render = true;
 
@@ -41,13 +55,17 @@ namespace BlazorWasmGames2.Pages
         protected override async Task OnInitializedAsync()
         {
             _sudokuGame = new SudokuGame();
-            _sudokuUi = _sudokuGame.GetSudokuUi();
+            //_sudokuUi = _sudokuGame.GetSudokuUi();
+            UpdateUISizeBindings();
+            _sudokuUi.GridSize = _sudokuUi.RowsBlock * _sudokuUi.ColsBlock;
 
             await Task.FromResult(0);
         }
 
-        void UpdateUIBindings()
+        void UpdateUISizeBindings()
         {
+            _sudokuUi.RowsBlock = _sudokuGame.GetRowsBlock();
+            _sudokuUi.ColsBlock = _sudokuGame.GetColsBlock();
         }
 
     }
