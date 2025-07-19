204fe1503e3323c4ecf989ab7ffae97f5ec8f4e8
Jeetendra Prasad <jeetendraprasad@users.noreply.github.com>
2025-07-20 03:02:01 +0530

    cource completed.

---
+++ Youtube/DotNet Real world example/hwzlFSLNVJ4/BlazorAppQuiz/BlazorAppQuiz/QuizQuestion.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorAppQuiz
+{
+    public class QuizQuestion
+    {
+        public string Question { get; set; } = string.Empty;
+        public List<string> Options { get; set; } = new List<string>();
+        public int CorrectAnswer { get; set; }
+    }
+}
